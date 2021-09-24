8d08c421262ff3428644942b2aac69656f12595d
Aashir Hassan Riaz <53640986+aashir844763@users.noreply.github.com>
2021-09-24 11:39:00 +0500

    initiating bot

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord');
+const client = new Discord.client();
+const token = require("./info.json");
+const prefix="$";
+
+
+
+client.on(message, async(message) =>{
+
+});
+
+client.login(token);
